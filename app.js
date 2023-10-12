90e1ac5e9b5b19e67ca4704370ebf398dd7ce273
Heriberto Rodriguez <bray@Heris-MacBook-Pro.local>
2023-10-11 19:58:08 -0700

    Installed pug a template engine. #21

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.set('views', './templates');
+app.set('view engine', 'pug');
+
 
 app.use(express.static(path.join(__dirname, 'templates')));
 app.use(express.static(path.join(__dirname, 'static')));
@@ -9,5 +12,9 @@ app.use(express.static(path.join(__dirname, '.')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static/index.html'));
 });
+
+app.get('/student', (req, res) => {
+    res.render('student', { title: 'Hey', message: 'Hi!' })
+  })
 
 app.listen(8000, () => console.log('Listening on port 8000...'));
