15516501ebd4c30bc933fd7d7ff954987e47d40a
Heriberto Rodriguez <bray@Heris-MacBook-Pro.local>
2023-11-11 16:40:46 -0800

    Separated script that creates tables from script that add students to DB and modified DB because of roster changes. #2

---
+++ db/create_tables.sql
@@ -0,0 +1,10 @@
+-- create tables
+CREATE TABLE fellows (id TEXT PRIMARY KEY NOT NULL, 
+                      name TEXT NOT NULL,
+					  email TEXT NOT NULL);
+CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+					    fellow_id TEXT NOT NULL,
+					    name TEXT NOT NULL, 
+					    period INTEGER NOT NULL, 
+					   sheets_row INTEGER NOT NULL,
+					   FOREIGN KEY (fellow_id) REFERENCES fellows(id));
