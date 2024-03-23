170137d2cdf567501d4520fea3345c373954f812
Heriberto Rodriguez <bray@Heris-MacBook-Pro.local>
2024-03-23 12:08:03 -0700

    Removed webpack config file and added an endpoint to get public ipv4 address of ec2 instance. #51

---
+++ routes/aws.js
@@ -0,0 +1,34 @@
+router.get('/aws/ec2-public-ipv4', (req, res) => {
+    const token='curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"';
+
+    const options = {
+        hostname: 'http://169.254.169.254',
+        port: 80,
+        path: '/latest/meta-data/public-ipv4',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-aws-ec2-metadata-token': token,
+        },
+    };
+
+    const req = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+            res.send(chunk);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+        res.send(`problem with request: ${e.message}`);
+    });
+
+    req.end();
+});
