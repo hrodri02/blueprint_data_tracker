cd4e6e04eae6836c668854db89aaa2c1f0d04c63
Heriberto Rodriguez <bray@Heris-MacBook-Pro.local>
2024-05-31 13:22:24 -0700

    Sending a different page depending on where the user is in the sign up flow. #57

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const db = require('../db/database');
 
 router.get('/', (req, res) => {
-    if (req.session.user) {
-        res.sendFile(path.join(__dirname, '../public/students.html'));  
+    const user = req.session.user;
+    if (user) {
+        if (user.sheets_permissions && user.sheet_id) {
+            const filePath = path.join(__dirname, '../public/students.html');
+            res.sendFile(filePath);  
+        }
+        else if (user.sheets_permissions)
+        {
+            res.sendFile(path.join(__dirname, '../public/sheet_url.html'));
+        }
+        else {
+            res.sendFile(path.join(__dirname, '../public/sheets_permissions.html'));
+        }
     }
     else {
         res.sendFile(path.join(__dirname, '../public/signup.html'));
