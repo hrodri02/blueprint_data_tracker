5bf7bea52ee64b9e9dd7375678b1f5a264dbcde9
Heriberto Rodriguez <bray@Heris-MacBook-Pro.local>
2024-06-27 08:29:43 -0700

    Replaced color column with text_color and background_color. #61

--- db/add_timers.sql
+++ db/add_timers.sql
@@ -1,2 +1,2 @@
-INSERT INTO timers(name, minutes, color, order_id, timers_collections_id) VALUES('Breakfast', 5, 0, 0, 2);
-INSERT INTO timers(name, minutes, color, order_id, timers_collections_id) VALUES('Search and Destroy', 17, 0, 1, 2);
+INSERT INTO timers(name, minutes, text_color, background_color, order_id, timers_collections_id) VALUES('Breakfast', 5, '#ccc', 'black', 2);
+INSERT INTO timers(name, minutes, text_color, background_color, order_id, timers_collections_id) VALUES('Search and Destroy', 17, '#ccc', 'black', 2);
