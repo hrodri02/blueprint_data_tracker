e999c6bb9b650967cc381606c4e51070567ff2ba
Heri <hrodriguez1821@gmail.com>
2024-07-05 12:26:19 -0700

    Moved logs to a folder. #68

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,8 @@
 require('express-async-errors');
 const winston = require('winston');
-winston.add(new winston.transports.File({filename: 'logfile.log', handleRejections: false, handleExceptions: false}));
-winston.exceptions.handle(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
-winston.rejections.handle(new winston.transports.File({ filename: 'uncaughtRejections.log' }));
+winston.add(new winston.transports.File({filename: './logs/logfile.log', handleRejections: false, handleExceptions: false}));
+winston.exceptions.handle(new winston.transports.File({ filename: './logs/uncaughtExceptions.log' }));
+winston.rejections.handle(new winston.transports.File({ filename: './logs/uncaughtRejections.log' }));
 
 module.exports = function (err, req, res, next) {
     winston.error(err.message, err);
