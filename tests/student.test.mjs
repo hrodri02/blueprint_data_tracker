a172d49565b7683469728f94e40d7e2f20ee3d19
Heri <hrodriguez1821@gmail.com>
2024-07-17 14:46:35 -0700

    Added tests for sortParticipationGrades(). #73

--- tests/student.test.mjs
+++ tests/student.test.mjs
@@ -1,5 +1,6 @@
 import { hash } from '../hash.mjs';
 import { validateExitTicketGrade } from '../validate_student.mjs';
+import { sortParticipationGrades } from '../participation_letters.mjs';
 
 describe('hash', () => {
     it('should convert a string to a hex', () => {
@@ -15,5 +16,17 @@ describe('validateExitTicketGrade', () => {
 
     it('should throw an error if the value is greater than 4', () => {
         expect(() => validateExitTicketGrade(5)).toThrow();
+    });
+});
+
+describe('sortParticipationGrades', () => {
+    it("should return ['GRADES'] if input is 'RADEGS'", () => {
+        const result = sortParticipationGrades('RADEGS');
+        expect(result).toEqual(['GRADES']);
+    });
+
+    it("should return [] if input is ''", () => {
+        const result = sortParticipationGrades('');
+        expect(result).toEqual([]);
     });
 });
