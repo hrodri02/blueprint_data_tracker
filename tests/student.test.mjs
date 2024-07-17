afaa76fd3f11ffb3f1f8a5da9c68e6927e6c3d05
Heri <hrodriguez1821@gmail.com>
2024-07-17 13:46:40 -0700

    Added a tests for validateExitTicketGrade(). #73

--- tests/student.test.mjs
+++ tests/student.test.mjs
@@ -1,8 +1,19 @@
 import { hash } from '../hash.mjs';
+import { validateExitTicketGrade } from '../validate_student.mjs';
 
 describe('hash', () => {
     it('should convert a string to a hex', () => {
         const result = hash('a');
         expect(result).toMatch(/^[a-fA-F0-9]+$/);
+    });
+});
+
+describe('validateExitTicketGrade', () => {
+    it('should throw an error if the value is less than 0', () => {
+        expect(() => validateExitTicketGrade(-1)).toThrow();
+    });
+
+    it('should throw an error if the value is greater than 4', () => {
+        expect(() => validateExitTicketGrade(5)).toThrow();
     });
 });
